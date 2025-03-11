3281a39bdf5d0253cf3952a799f2e8848be7a6b6
Binod Gautam <kanika@bhangar.net>
2025-03-11 21:07:37 +0545

    feat: add database connection

---
+++ src/infrastructure/database/pgSql/data-source.ts
@@ -0,0 +1,24 @@
+import { Pool } from "pg";
+import { Config } from "../../../config";
+import logger from "../../../config/logger";
+
+const pool=new Pool({
+    user: Config.DB_USER,
+    host: Config.DB_HOST,
+    database: Config.DB_NAME,
+    password: Config.DB_PASSWORD,
+    port: parseInt(Config.DB_PORT!),
+})
+
+export const testDbConnection= async()=>{
+    try {
+         const client=await pool.connect()
+        logger.info("Database Connected Successfully")
+        client.release()
+    } catch (error) {
+        logger.error("Database connection failed:",error)
+    }
+}
+
+
+export default pool
