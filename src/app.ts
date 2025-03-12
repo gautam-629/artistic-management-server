8d6d1635c6b096bea34dd01d3f98be76d7675aed
Binod Gautam <kanika@bhangar.net>
2025-03-12 21:55:28 +0545

    feat: implement api endpoint for creating a user

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,32 @@
-import http from 'http';
+import http, { IncomingMessage, ServerResponse } from 'http';
 import { testDbConnection } from './config/data-source';
+import { UserController } from './users/users.controller';
+import { Userservice } from './users/users.service';
 
-const app = http.createServer((req, res) => {
-  res.writeHead(200, { 'Content-Type': 'application/json' });
-  res.end(JSON.stringify({ message: "Hello from Server" }));
+const userController=new UserController(new Userservice())
+
+const app = http.createServer(async (req:IncomingMessage, res:ServerResponse) => {
+
+  const path=req.url?.split('/')[1];
+
+  //cors
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+
+  if (req.method === 'OPTIONS') {
+    res.writeHead(200)
+    res.end()
+    return;
+  }
+
+  if (path === 'users') {
+    await userController.handleRequest(req, res);
+  } else {
+    res.writeHead(404, { 'Content-Type': 'application/json' });
+    res.end(JSON.stringify({ error: 'Not Found' }));
+  }
+
 });
 
 testDbConnection()
---
+++ src/common/util/requestUtils.ts
@@ -0,0 +1,22 @@
+export async function getRequestBody(req: any) {
+  return new Promise<any>((resolve, reject) => {
+    let body = '';
+    
+    req.on('data', (chunk: string) => {
+      body += chunk;
+    });
+
+    req.on('end', () => {
+      try {
+        const parsedBody = JSON.parse(body || '{}');
+        resolve(parsedBody);
+      } catch (error) {
+        reject(new Error('Invalid JSON format'));
+      }
+    });
+
+    req.on('error', (error: any) => {
+      reject(new Error('Error reading request body'));
+    });
+  });
+}
